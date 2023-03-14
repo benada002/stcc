9ac2955dc2c923ebb8330a1e9900ecdf73bd5693
Benedict Adams (benada002) <45796304+benada002@users.noreply.github.com>
2023-03-14 20:28:08 +0100

    Update queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  QueryClient,
-} from '@tanstack/react-query';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
-import { compress, decompress } from 'lz-string';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App';
+import { CACHE_MAX_AGE } from './constants';
+import {
+  queryClient,
+  persister,
+} from './queryClient';
 
 import 'antd/dist/reset.css';
 import './index.css';
-import { CACHE_MAX_AGE } from './constants';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      cacheTime: CACHE_MAX_AGE,
-      retry: 2,
-      retryDelay: 5000,
-    },
-  },
-});
-
-const localStoragePersister = createSyncStoragePersister({
-  storage: window.localStorage,
-  serialize: (data) => compress(JSON.stringify(data)),
-  deserialize: (data) => JSON.parse(decompress(data)),
-});
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <PersistQueryClientProvider
       client={queryClient}
       persistOptions={{
-        persister: localStoragePersister,
+        persister,
+        maxAge: CACHE_MAX_AGE,
       }}
     >
       <App />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </PersistQueryClientProvider>
   </React.StrictMode>,
 );
